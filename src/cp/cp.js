747124801eff4404ad514764d71e4f2aa1d26643
KotoJeep <omaigader@gmail.co>
2022-12-29 23:27:39 +0700

    feat: add resolve cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,25 @@
+import {spawn} from 'child_process'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+const __dirName = dirname(fileURLToPath(import.meta.url))
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  let init = false
+  const chProcess = spawn('node', [`${__dirName}/files/script.js`, ...args.split(' ')])
+  
+  process.stdin.on('data', msg => {
+    chProcess.stdin.write(msg)
+  })
+  
+  chProcess.stdout.on('data', data => {
+    console.log(data.toString())
+    if (!init) {
+      console.log(`Write anything to console` + '\n')
+      init = true
+    }
+  })
+  
 };
 
 spawnChildProcess();
